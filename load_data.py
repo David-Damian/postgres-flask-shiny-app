be5b8538b78ce3d71c23899d2ba996ac125463dd
Miguel Castaneda <miguel.castama@gmail.com>
2022-11-24 12:08:14 -0600

    Cleaning initial data load file

--- load_data.py
+++ load_data.py
@@ -1,68 +1,29 @@
-import os
-import csv
-import numpy as np
+import db
 import psycopg2
 from flask import Flask
 
-
-def create_query(query_file):
-    """"
-    Esta función recibe un archivo .txt con un query
-    de SQL para transformarlo en un str de python.
-    
-    Parámetros:
-    query_file: Archivo .txt con query de SQL.
-    """
-    #Se abre el archivo
-    with open(query_file, mode='r') as file:
-        query = """"""
-        for row in file:
-            query += (row)      
-    file.close()
-    return query
-
 # Get environment variables
-USER = os.getenv('POSTGRES_USER')
-PASSWORD = os.environ.get('POSTGRES_PASSWORD')
-ADDRESS = os.environ.get('POSTGRES_ADDR')
-DB = os.environ.get('POSTGRES_DB')
-print(f"USER: {USER}, PASS: {PASSWORD}, ADDRESS: {ADDRESS}, DB: {DB}")
+USER = db.USER
+PASSWORD = db.PASSWORD
+ADDRESS = db.ADDRESS
+DB = db.DB
+print(f"USER: {USER}, PASS: {PASSWORD}, ADDRESS: {ADDRESS}, DB: {DB}\n")
 
 #Conexión a base de datos
 conn = psycopg2.connect(
     database=DB,
     user=USER,
     password=PASSWORD,
-    host=ADDRESS
-)
-
-#Cursor para operaciones en base de datos
+    host=ADDRESS)
 cur = conn.cursor()
 
 #Lectura e inserción de datos en bruto
 schema, table = 'wine', 'wine_quality'
+data_file = 'datos/winequality-red-raw.csv'
+query_template = 'db/insert_data.txt'
+
 print(f"Insertando datos en {table}...")
-with open('datos/winequality-red-raw.csv', mode='r') as file:
-    csvFile = csv.reader(file)
-    
-    for line in csvFile:
-        try:
-            #Se truncan valores a 6 decimales max
-            row = np.array(line).astype(float) * 1e6
-            row = np.trunc(row) / 1e6
-            
-            #Query de inserción
-            insert_query = create_query("db/insert_data.txt")
-            insert_query = insert_query.replace("<schema>", f"{schema}").replace("<table>", f"{table}")
-            for i in range(len(row)):
-                insert_query = insert_query.replace(f"x_{i}", f"{row[i]}")
-            
-            #Escritura en base de datos
-            cur.execute(insert_query)
-            
-        except ValueError:
-            pass
-file.close()
+db.insert_data(cursor=cur, schema=schema, table=table, data=data_file, template=query_template)
 print("Datos insertados.")
 
 #Consulta de inserción en base de datos
