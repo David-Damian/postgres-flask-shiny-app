63337edb40b9be5540aa508da47d67db12edd7c0
Navarreteed <eduardo.gutierreznavarrete@gmail.com>
2022-12-07 14:26:54 -0600

    se arregla el delete

--- docker-shiny/vinos-app/server.R
+++ docker-shiny/vinos-app/server.R
@@ -1,4 +1,35 @@
 shinyServer(function(input, output) {
+
+#wine_data <- read_csv("winequality-red-raw.csv")
+url <- 'web:4999/'
+
+response <- GET(url) #"https://geeksforgeeks.org"
+wine_data <- fromJSON(content(response, as='text'))
+
+#Variables como factor
+wine_data$quality <- as.factor(wine_data$quality)
+wine_data$type <- as.factor(wine_data$type)
+
+#Variables como double
+wine_data$density <- as.double(wine_data$density)
+wine_data$fixed_acidity <- as.double(wine_data$fixed_acidity)
+wine_data$volatile_acidity <- as.double(wine_data$volatile_acidity)
+wine_data$citric_acid <- as.double(wine_data$citric_acid)
+wine_data$residual_sugar <- as.double(wine_data$residual_sugar)
+wine_data$chlorides <- as.double(wine_data$chlorides)
+wine_data$free_sulfur_dioxide <- as.double(wine_data$free_sulfur_dioxide)
+wine_data$ph <- as.double(wine_data$ph)
+wine_data$sulphates <- as.double(wine_data$sulphates)
+wine_data$alcohol <- as.double(wine_data$alcohol)
+wine_data$total_sulfur_dioxide <- as.double(wine_data$total_sulfur_dioxide)
+
+
+
+
+
+
+
+
   #recibe las caracteristicas de un nuevo vino para predecir su calidad:
   observeEvent(input$confirmar,{
         POST('web:4999/predict', body=toJSON(data.frame(
@@ -19,6 +50,14 @@ shinyServer(function(input, output) {
   POST('web:4999/delete', body=toJSON(data.frame(
     caract_delid = input$IDreg_delete))
   )
+  url <- 'web:4999/'
+
+  response <- GET(url) #"https://geeksforgeeks.org"
+  wine_data <- fromJSON(content(response, as='text'))
+
+  #Variables como factor
+  wine_data$quality <- as.factor(wine_data$quality)
+  wine_data$type <- as.factor(wine_data$type)
   })
 
  observeEvent(input$predecir,{
@@ -89,3 +128,4 @@ shinyServer(function(input, output) {
     })
   
   })
+
