58845e5dbab71582503612408d661825073e21e0
Miguel Castaneda <miguel.castama@gmail.com>
2022-12-07 00:21:52 -0600

    predict service POST GET

--- api/app.py
+++ api/app.py
@@ -3,7 +3,7 @@ import json
 import numpy as np
 import psycopg2
 import psycopg2.extras
-from flask import Flask
+from flask import Flask, request
 from utils import predict_quality
 
 #Get enviroment variables
@@ -20,7 +20,7 @@ conn = psycopg2.connect(database_uri)
 app = Flask(__name__)
 
 @app.route('/')
-@app.route("/inicio", methods=['GET', 'POST'])
+@app.route("/inicio", methods=['GET'])
 def inicio():
     cur = conn.cursor(cursor_factory=psycopg2.extras.NamedTupleCursor)
     cur.execute(f"SELECT * FROM {schema}.{table}")
@@ -30,28 +30,44 @@ def inicio():
 
 @app.route("/predict", methods=['GET', 'POST'])
 def predict():
-    X_play = np.array([[6.0000e+00, 2.1000e-01, 3.8000e-01, 8.0000e-01, 2.0000e-02,
-        2.2000e+01, 9.8000e+01, 9.8941e-01, 3.2600e+00, 3.2000e-01,
-        1.1800e+01]])
-    cur = conn.cursor(cursor_factory=psycopg2.extras.NamedTupleCursor)
-    cur.execute(f"""SELECT
-        fixed_acidity,
-        volatile_acidity,
-        citric_acid,
-        residual_sugar,
-        chlorides,
-        free_sulfur_dioxide,
-        total_sulfur_dioxide,
-        density,
-        ph,
-        sulphates,
-        alcohol,
-        quality
-    FROM {schema}.{table}""")
-    rows = np.array(cur.fetchall())
-    results = predict_quality(rows, X_play)
-    cur.close()
-    return str(results)
+    
+    
+    if request.method == 'POST':
+        x_input = json.loads(request.data)
+        x_input = np.array([list(x_input[0].values())])
+
+        cur = conn.cursor(cursor_factory=psycopg2.extras.NamedTupleCursor)
+        cur.execute(f"""SELECT
+            fixed_acidity,
+            volatile_acidity,
+            citric_acid,
+            residual_sugar,
+            chlorides,
+            free_sulfur_dioxide,
+            total_sulfur_dioxide,
+            density,
+            ph,
+            sulphates,
+            alcohol,
+            quality
+        FROM {schema}.{table}""")
+        rows = np.array(cur.fetchall())
+        results = predict_quality(rows, x_input)
+        cur.close()
+
+        # Open a file
+        fo = open("results.txt", "w")
+        fo.write(f"Prediction: {str(results)}")
+        fo.close()
+
+        return str(results)
+    
+    if request.method == 'GET':
+        file = open("results.txt", "r")
+        results = file.read()
+        results.close()
+
+        return str(results)
 
 
 if __name__ == '__main__':
