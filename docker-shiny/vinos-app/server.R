893ddf74b43f1017b7c042930a0b62a9b54320b2
Miguel Castaneda <miguel.castama@gmail.com>
2022-12-07 19:50:06 -0600

    Remove double submit on update

--- docker-shiny/vinos-app/server.R
+++ docker-shiny/vinos-app/server.R
@@ -80,23 +80,6 @@ wine_data$total_sulfur_dioxide <- as.double(wine_data$total_sulfur_dioxide)
   output$prediccion_text = renderPrint(df)
   })
 
-
-  #MANDA a la API, caracteristicas de un nuevo vino 
-  observeEvent(input$submit,{
-        POST('web:4999/submit', body=toJSON(data.frame(
-          caract_dummy1=input$acidezfija,
-          caract_dummy2=input$acidezvolatil, 
-          caract_dummy3=input$acidcitric, 
-          caract_dummy4=input$azucarresidual,
-          caract_dummy5=input$cloruros,
-          caract_dummy6=input$so4libre,
-          caract_dummy7=input$so4total,
-          caract_dummy8=input$densidad,
-          caract_dummy9=input$PH,
-          caract_dummy10=input$sulfatos,
-          caract_dummy11=input$alcohol)))
-    }) 
-
     
 #Tab Datos
     #Tabla
